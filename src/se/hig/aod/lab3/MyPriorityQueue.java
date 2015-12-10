2a99bd55afdd94b077b2aeb70dd37fea7f2c249a
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-12-10 23:56:46 +0100

    Update exceptions

--- src/se/hig/aod/lab3/MyPriorityQueue.java
+++ src/se/hig/aod/lab3/MyPriorityQueue.java
@@ -74,7 +74,16 @@ public interface MyPriorityQueue<T extends Comparable<? super T>>
     @SuppressWarnings("serial")
     public class MyPriorityQueueException extends RuntimeException
     {
-
+        /**
+         * Create a new exception
+         * 
+         * @param message
+         *            the reason for this exception
+         */
+        public MyPriorityQueueException(String message)
+        {
+            super(message);
+        }
     }
 
     /**
@@ -86,7 +95,16 @@ public interface MyPriorityQueue<T extends Comparable<? super T>>
     @SuppressWarnings("serial")
     public class MyPriorityQueueIsFullException extends MyPriorityQueueException
     {
-
+        /**
+         * Create a new exception
+         * 
+         * @param message
+         *            the reason for this exception
+         */
+        public MyPriorityQueueIsFullException(String message)
+        {
+            super(message);
+        }
     }
 
     /**
@@ -98,7 +116,16 @@ public interface MyPriorityQueue<T extends Comparable<? super T>>
     @SuppressWarnings("serial")
     public class MyPriorityQueueIsEmptyException extends MyPriorityQueueException
     {
-
+        /**
+         * Create a new exception
+         * 
+         * @param message
+         *            the reason for this exception
+         */
+        public MyPriorityQueueIsEmptyException(String message)
+        {
+            super(message);
+        }
     }
 
     /**
@@ -109,6 +136,35 @@ public interface MyPriorityQueue<T extends Comparable<? super T>>
     @SuppressWarnings("serial")
     public class MyPriorityQueueNullNotAllowedException extends MyPriorityQueueException
     {
-
+        /**
+         * Create a new exception
+         * 
+         * @param message
+         *            the reason for this exception
+         */
+        public MyPriorityQueueNullNotAllowedException(String message)
+        {
+            super(message);
+        }
+    }
+    
+    /**
+     * An exception that SHOULD NOT HAPPEN
+     *
+     * @author Viktor Hanstorp (ndi14vhp@student.hig.se)
+     */
+    @SuppressWarnings("serial")
+    public class MyPriorityQueueInternalException extends MyPriorityQueueException
+    {
+        /**
+         * Create a new exception
+         * 
+         * @param message
+         *            the reason for this exception
+         */
+        public MyPriorityQueueInternalException(String message)
+        {
+            super(message);
+        }
     }
 }
